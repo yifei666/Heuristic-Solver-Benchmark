fcaa8f12c213163af005a02dd8433b97198cffe4
yifei666 <43050198+yifei666@users.noreply.github.com>
2022-01-24 23:02:35 -0500

    Add files via upload

---
+++ randomgraph.py
@@ -0,0 +1,262 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  4 10:18:25 2021
+
+@author: Michael
+"""
+import time
+from networkx.generators.random_graphs import erdos_renyi_graph
+import networkx as nx
+import random
+import operator
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+from solver import solvermethod
+
+def sorting_allpath(allpath_list, g):
+    allpath_dict = {}
+    output = []
+    for path in allpath_list:
+        total_length = 0
+        total_latency = 0
+        for i in range(len(path)-1):
+            source, target = path[i], path[i+1]
+            edge = g[source][target]
+            length = edge['weight']
+            lat = edge['latency']
+            total_length += length
+            total_latency += lat
+        allpath_dict[(total_latency,total_length)] = path
+        
+        output = allpath_dict.items()
+        
+    return output
+    
+
+def latencyheur(g, nodes, max_latency):
+    global solution
+        #### creating the heuristic model
+    time_start=time.time()
+    allpath_list=[]
+
+    for path in nx.all_simple_paths(g, source=0, target=nodes-1):
+        allpath_list.append(path)
+
+    sortedlist = sorting_allpath(allpath_list, g)
+    solution = "Null"
+    for pair in sortedlist:
+        if pair[0][0] <= max_latency:
+            solution = pair
+            break
+            
+    time_end = time.time()
+    time_used = (time_end-time_start)*1000
+    
+    return [solution,time_used]
+
+def zerolistmaker(n):
+    listofzeros = [0] * n
+    return listofzeros
+
+
+def bwassign(g): ## pass in the bw name
+    for (u,v,w) in g.edges(data=True):
+        w['bandwidth'] = random.randint(1,150)
+
+
+ ## also for latency filter
+def bwfilter(g,bwlimit): ## remove paths that does not satisfy the bw requirement and display paths that has been removed
+    path_remove = []
+    path_remove_withbw = []
+    for (u,v,w) in g.edges(data=True):
+        if w['bandwidth'] < bwlimit:
+            path_remove.append((u,v))
+            path_remove_withbw.append((u,v,w))
+    g.remove_edges_from(path_remove)
+    print("remove path:" + str(path_remove_withbw))
+    return g
+    
+        
+def weightassign(g):
+    distance_list = []
+    latency_list = []
+    for (u,v,w) in g.edges(data=True):
+        w['weight'] = random.randint(1,2**24) #10-10^9
+        
+        latency = random.randint(10,100) #10,100 1/100 msec
+        w['latency'] =latency
+        distance_list.append(w['weight']) 
+        latency_list.append(latency) 
+    return[distance_list,latency_list]
+
+def nodes_connected(g, u, v):
+    return u in g.neighbors(v)
+
+def jsonfilemaker(nodes, inputmatrix, inputdistance, link_list, max_latency, sortedanswer):
+    rhs = zerolistmaker(nodes)
+    rhs[0] = -1
+    rhs[-1] = 1    
+    jsonoutput = {}
+    jsonoutput['constraint_coeffs'] = inputmatrix
+    jsonoutput['bounds'] = rhs
+    jsonoutput['obj_coeffs'] = inputdistance
+    jsonoutput['num_vars'] = len(link_list)
+    jsonoutput['num_constraints'] = len(inputmatrix)
+    jsonoutput['max_latency'] = max_latency
+    with open('data.json', 'w') as json_file:
+        json.dump(jsonoutput, json_file,indent=4)
+  
+    try:
+        output = sortedanswer[0][1]
+    except IndexError:
+        output = "Null"
+    return [output,time]
+    
+def nxgraphgenerator(nodes,p,max_latency,bwlimit):
+    # random.seed(seed)
+    g = erdos_renyi_graph(nodes,p)
+    
+    while True:
+        if nx.is_connected(g):
+            break
+        else:
+            g = erdos_renyi_graph(nodes,p)
+    bwassign(g)
+    bwfilter(g, bwlimit)
+
+        
+    link_dict = {}
+    weightassignment = weightassign(g)
+    edgelist = list(g.edges)
+    
+    ## generate each node's parent node
+    for pair in g.edges:
+        if pair[0] in link_dict:
+            link_dict[pair[0]].append(pair[1])
+        else:
+            link_dict[pair[0]]=[pair[1]]
+        
+        if pair[1] in link_dict:
+            link_dict[pair[1]].append(pair[0])
+        else:
+            link_dict[pair[1]]=[pair[0]]
+    linknum = 2*len(g.edges)
+         
+    sorted_dict = dict(sorted(link_dict.items(), key=operator.itemgetter(0)))  
+
+    
+    
+    ## show every link (bidirectional link means 2 different link)
+    link_list = []
+    for startnode in sorted_dict:
+        for endnode in sorted_dict[startnode]:
+            link_list.append([startnode, endnode])
+            
+    
+    ## generte a link name list for future look up and reference
+    linktitle_dict={}
+    for n in range(len(link_list)):
+        linktitle_dict[n] = link_list[n]
+
+    
+    ## create the  contraint matrix of 0s
+    nodenum = len(g.nodes)    
+    inputmatrix = []
+    for n in range(nodenum):
+        inputmatrix.append(zerolistmaker(linknum))
+
+    
+    ## input values based on the linklist into the matrix, 1 means flow into the nodes, -1 meanse flow out of the node
+    c = 0
+    for line in inputmatrix:
+        n = 0
+        for link in link_list:
+            if link[0] == c:
+                inputmatrix[c][n] = -1
+                n= n+1
+            elif link[1] == c:
+                inputmatrix[c][n] = 1
+                n=n+1
+            else:
+                n=n+1
+        c = c+1
+    
+    
+    inputdistance = zerolistmaker(len(link_list))
+    inputlatency = zerolistmaker(len(link_list))
+    distance_list = weightassignment[0]
+    latency_list = weightassignment[1]
+
+    
+    ## look up and form the distance and latency array for each link
+    count = 0
+    for link in link_list:
+        try:
+            inputdistance[count] = distance_list[edgelist.index((link[0],link[1]))]
+            count = count+1
+        except ValueError:
+            inputdistance[count] = distance_list[edgelist.index((link[1],link[0]))]
+            count = count+1
+            
+    count = 0        
+    for link in link_list:
+        try:
+            inputlatency[count] = latency_list[edgelist.index((link[0],link[1]))]
+            count = count+1
+        except ValueError:
+            inputlatency[count] = latency_list[edgelist.index((link[1],link[0]))]
+
+    pos = nx.spring_layout(g)
+    
+    print()
+    
+
+    # Draw the graph according to node positions
+    labels = nx.get_edge_attributes(g,'bandwidth')
+    print("labels" + str(labels))
+    # nx.draw(g, pos, with_labels=labels)
+    # nx.draw_networkx_edge_labels(g, pos, edge_labels=labels)
+    latencyheurresult = latencyheur(g,nodes,max_latency)
+    sortedanswer = latencyheurresult[0]
+    time = latencyheurresult[1]
+    
+    print("sorted answer: "+str(sortedanswer))
+    
+ 
+    
+    rhs = zerolistmaker(nodes)
+    rhs[0] = -1
+    rhs[-1] = 1    
+    jsonoutput = {}
+    jsonoutput['constraint_coeffs'] = inputmatrix
+    jsonoutput['bounds'] = rhs
+    jsonoutput['obj_coeffs'] = inputdistance
+    jsonoutput['num_vars'] = len(link_list)
+    jsonoutput['num_constraints'] = len(inputmatrix)
+    jsonoutput['max_latency'] = max_latency
+    with open('data.json', 'w') as json_file:
+        json.dump(jsonoutput, json_file,indent=4)
+  
+    try:
+        output = sortedanswer[0][1]
+    except IndexError:
+        output = "Null"
+    return [output,time]
+
+
+
+
+
+
+
+try:
+    heur = nxgraphgenerator(8,0.4,1000,20)
+    print(heur)
+except UnboundLocalError:
+    print("The source and destination nodes are no longer connected. ")
+    
+
+
+
+
---
+++ test.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 25 15:13:44 2021
+
+@author: yifeiwang
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 25 14:28:47 2021
+
+@author: yifeiwang
+"""
+from solver import solvermethod
+from randomgraph import nxgraphgenerator
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+
+def filesave(file):
+    with open('graphdata2.json', 'w') as json_file:
+        json.dump(file, json_file,indent=4)
+        
+    
+def draw_errorbar(nodes, avglist, std, name):
+    labels = []
+    x_axis = []
+    for num in range(nodes[0],nodes[1]):
+        labels.append(str(num))
+        x_axis.append(num)
+
+    x_pos = np.arange(len(labels))
+    fig, ax = plt.subplots()
+    ax.bar(x_pos, avglist,
+           yerr=std,
+           align='center',
+           alpha=0.5,
+           ecolor='black',
+           capsize=10)
+
+    ax.set_xticks(x_pos)
+    ax.set_xticklabels(labels)
+    ax.set_title('Heuristic Std Error')
+    ax.yaxis.grid(True)
+    ax.set_xlabel("Number of Nodes")
+    ax.set_ylabel('Path Weights')
+    
+
+    plt.tight_layout()
+    plt.savefig('Heuristic Std Error.png', dpi = 300)
+    plt.show()
+
+def draw_comparisionbarplot(nodes, solvertimelist, heurtimelist):
+    labels = []
+    x_axis = []
+    for num in range(nodes[0],nodes[1]):
+        labels.append(str(num))
+        x_axis.append(num)
+
+
+    plt.plot(x_axis, solvertimelist, label = "solver")
+    plt.plot(x_axis, heurtimelist, label = "heur")
+    plt.xlabel("Number of Nodes")
+    plt.ylabel("Milliseconds")
+    plt.title("Computation Time: Solver Vs Heuristic")
+    plt.legend()
+    plt.savefig("Computatoin Time", dpi = 300)
+    plt.show()
+
+def draw_weightcomparison(nodes, heuravglist, solveravglist):
+        
+    Y = heuravglist
+    Z = solveravglist
+      
+    X = list(range(nodes[0],nodes[1]))
+    
+    X_axis = np.arange(len(X))
+      
+    plt.bar(X_axis - 0.2, Y, 0.4, label = 'Heuristic Solution')
+    plt.bar(X_axis + 0.2, Z, 0.4, label = 'Solver Solution')
+      
+    plt.xticks(X_axis, X)
+    plt.xlabel("Nodes")
+    plt.ylabel("NWeights")
+    plt.title("Heuristic VS Solver (Sorted by latency)")
+    plt.legend()
+    plt.savefig('Weights comparision.png', dpi = 300)
+    plt.show()
+    
+
+def draw_errorplotweightplot(nodes,heuravglist, solveravglist, heurstd, solstd):
+        
+    Y = heuravglist
+    Z = solveravglist
+    Yerr = heurstd
+    Zerr = solstd
+    
+    x_axis = np.linspace(nodes[0],nodes[1]-1, nodes[1]-nodes[0])
+    
+    fig, ax = plt.subplots()
+    
+    
+    ax.errorbar(x_axis, Y,
+                yerr=Yerr,
+                fmt='-o',label="Heuristic")
+    ax.errorbar(x_axis, Z,
+                yerr=Zerr,
+                fmt='-o',label = "Solver")
+    
+    ax.set_xlabel('Nodes')
+    ax.set_ylabel('Weight')
+    ax.set_title('Weight plot with error bars')
+    plt.legend()
+    plt.savefig("errorbar.png", dpi=300)
+    
+    plt.show()
+    
+def graphgenerator(): 
+    with open('graphdata2.json') as f:
+          file = json.load(f)
+    solvertimelist = file["solvertimelist"]   
+    heurtimelist = file["heurtimelist"]
+    solveravglist = file["solveravglist"]
+    heuravglist = file["heuravglist"]
+    heurstd = file["heurstd"]
+    solstd = file["solstd"]
+    nodes = file["nodes"]
+
+    draw_errorbar(nodes, heuravglist, heurstd, 'Heuristic Std Error')
+    
+    draw_errorbar(nodes, solveravglist, solstd, 'Heuristic Std Error')
+    
+    draw_comparisionbarplot(nodes, solvertimelist, heurtimelist)
+
+    draw_weightcomparison(nodes, heuravglist, solveravglist)
+    
+    draw_errorplotweightplot(nodes,heuravglist, solveravglist, heurstd, solstd)
+    
+    
+    
+    
+def test(latency, nodes, n, samplenumber, coefficient, bwlimit):
+    data=[]
+    perlist=[]
+    for node in range(nodes[0],nodes[1]+1):
+        i = 0
+        while i < samplenumber:
+            heurresult = nxgraphgenerator(node, n, coefficient*node, bwlimit)
+            heur = heurresult[0]
+            heurtime = heurresult[1]
+            if heur != "Null":
+                solverresult = solvermethod()
+                solver = solverresult[0]
+                solvertime = solverresult[1]
+                if abs(heur- solver)> 10:
+                    errper = abs(heur - solver)/solver
+                    perlist.append(errper)
+                    data.append([solver,heur,solvertime,heurtime,node,i])
+                    i+=1
+    print("data:"+str(data))
+    solvertimelist = []
+    heurtimelist = []
+    solveravglist = []
+    heuravglist = []
+    heurstd = []
+    solstd = []
+    length = int(nodes[1]-nodes[0])
+    for n in range(length):
+        solvertime = []
+        heurtime = []
+        solversol = []
+        heursol= []
+        c = 0
+        while c < samplenumber:
+            solversol.append(data[n * samplenumber+c][0])
+            heursol.append(data[n * samplenumber+c][1])
+            solvertime.append(data[n * samplenumber+c][2])
+            heurtime.append(data[n * samplenumber+c][3])
+            c=c+1
+        solvertimelist.append(np.mean(solvertime))
+        heurtimelist.append(np.mean(heurtime))
+        solveravglist.append(np.mean(solversol))
+        heuravglist.append(np.mean(heursol))
+        solstd.append(np.std(solversol))
+        heurstd.append(np.std(heursol))
+
+    
+            
+            
+        
+        
+    print(solvertimelist)
+    print(heurtimelist)
+    print(solveravglist)
+    print(heuravglist)
+    print(heurstd)
+    print(solstd)
+    
+    file = {}
+    file['solvertimelist'] = solvertimelist
+    file['heurtimelist'] = heurtimelist
+    file['solveravglist'] = solveravglist
+    file['heuravglist'] = heuravglist
+    file['heurstd'] = heurstd
+    file['solstd'] = solstd
+    file["nodes"] = nodes
+    
+    filesave(file)
+    graphgenerator()
+
+
+
+test(nodes=[15,18],n=0.1,samplenumber=5, coefficient=7,latency = 1000, bwlimit = 20)
+
